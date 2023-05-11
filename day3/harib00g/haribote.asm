85d474f2727545010af8d7041bf7f87a9c018134
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-05-11 12:14:37 +0900

    3日目 harib00g 追加

---
+++ day3/harib00g/haribote.asm
@@ -0,0 +1,10 @@
+; hello-os
+; TAB=4
+
+		ORG		0xc200			; このプログラムがどこに読み込まれるのか
+        MOV     AL,0x13         ; VGAグラフィックス, 320x200 8bitカラー
+        MOV     AH,0x00
+        INT     0x10
+fin:
+		HLT
+		JMP 	fin
